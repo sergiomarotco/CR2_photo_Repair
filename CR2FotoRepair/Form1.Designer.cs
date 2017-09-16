ae88f9c7a2f5fa7407f2f769c40f5a413628889f
Marotco Sergio <Marotco Sergio>
2017-09-16 09:32:37 +0700

    Добавьте файлы проекта.

---
+++ CR2FotoRepair/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace CR2FotoRepair
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            resources.ApplyResources(this.openFileDialog1, "openFileDialog1");
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // linkLabel1
+            // 
+            resources.ApplyResources(this.linkLabel1, "linkLabel1");
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ CR2FotoRepair/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CR2FotoRepair
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog d = new OpenFileDialog()
+            {
+                DefaultExt = ".cr2",
+                Filter = "CR2 files (*.cr2)|*.cr2|All files (*.*)|*.*",
+                CheckFileExists = true,
+                InitialDirectory = Environment.SpecialFolder.Desktop.ToString()
+            };
+            DialogResult result = d.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                Goodfoto = d.FileName;
+            }
+        }
+
+        private string Goodfoto = @"C:\default.cr2";
+        private string folder = @"C:\";
+        private List<string> badFiles;
+        //private byte[] GoodfotoArray;
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog F = new FolderBrowserDialog()
+            {
+                Description = "Выберите папку с фотографиями *.cr2 которые необходимо восстановить",
+                ShowNewFolderButton = true,
+                RootFolder = Environment.SpecialFolder.Desktop
+            };
+            DialogResult result = F.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                if (Directory.Exists(F.SelectedPath))
+                {
+                    folder = F.SelectedPath;
+                }
+                else
+                {
+                    MessageBox.Show("Выбрана несуществующая папка");
+                }
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            badFiles = new List<string>();
+            badFiles.AddRange(System.IO.Directory.GetFiles(folder, "*.cr2"));
+            byte[] GoodfotoArray = File.ReadAllBytes(Goodfoto);
+            byte[] GoodfotoWithoutRight=new byte[1];
+  /**/     // GoodfotoArray = new byte[] { 109, 110, 111, 255, 216, 255, 196, 112, 113, 114,115 };
+            for (int j = 0; j < GoodfotoArray.Length; j++)
+            {
+                if (GoodfotoArray[j] == 255)
+                    if (GoodfotoArray[j + 1] == 216)
+                        if (GoodfotoArray[j + 2] == 255)
+                            if (GoodfotoArray[j + 3] == 196)
+                            {
+                                GoodfotoWithoutRight = new byte[j];
+                                Array.Copy(GoodfotoArray, 0, GoodfotoWithoutRight, 0, j);
+                                break;
+                            }
+            }
+            int NotRepairedFoto = 0;
+            string NotRepairedFotostring = "";
+            string newfilename = "";
+            for (int i = 0; i < badFiles.Count; i++)
+            {
+                try
+                {
+                    FileInfo ff = new FileInfo(badFiles[i]);
+                    Directory.CreateDirectory(ff.Directory.ToString() + @"\\repaired2");
+                    newfilename = ff.Directory + "\\" + ff.Name.Split('.')[0] + "..JPG";
+                    File.Copy(badFiles[i], newfilename, true);
+                    badFiles[i] = newfilename;
+                    byte[] BadfotoWithoutLeft = new byte[1];
+                    byte[] foto = File.ReadAllBytes(badFiles[i]);
+                    /**/            //foto = new byte[] { 209,210, 211, 255, 216, 255, 196, 255, 216, 255, 196, 212, 213,214,215 };
+                    List<int> jIndexes = new List<int>();
+                    for (int j = 0; j < foto.Length; j++)
+                    {
+                        if (foto[j] == 255)
+                            if (foto[j + 1] == 216)
+                                if (foto[j + 2] == 255)
+                                    if (foto[j + 3] == 196)
+                                        jIndexes.Add(j);
+
+                            jIndexes.Add(j);
+                    }
+                    if (jIndexes.Count != 0)
+                    {
+                        NotRepairedFoto++;
+                        BadfotoWithoutLeft = new byte[foto.Length - jIndexes[jIndexes.Count - 1]];
+                        Array.Copy(foto, jIndexes[jIndexes.Count - 1], BadfotoWithoutLeft, 0, foto.Length - jIndexes[jIndexes.Count - 1]);
+                        byte[] NewFoto = new byte[BadfotoWithoutLeft.Length + GoodfotoWithoutRight.Length];
+                        Array.Copy(GoodfotoWithoutRight, 0, NewFoto, 0, GoodfotoWithoutRight.Length);
+                        Array.Copy(BadfotoWithoutLeft, 0, NewFoto, GoodfotoWithoutRight.Length, BadfotoWithoutLeft.Length);
+                        File.WriteAllBytes(newfilename, NewFoto);
+                        File.Move(newfilename, ff.Directory + "\\repaired2\\" + ff.Name.Split('.')[0] + "......CR2");
+                        File.Delete(newfilename);
+                        newfilename = ff.Directory + "\\repaired2\\" + ff.Name.Split('.')[0] + "......CR2";
+                        File.WriteAllBytes(newfilename, NewFoto);
+                    }
+                    else
+                    { File.Delete(ff.Directory + "\\" + ff.Name.Split('.')[0] + "......CR2"); NotRepairedFotostring += ff.FullName + Environment.NewLine; }
+                }
+                catch (Exception ee) { MessageBox.Show(ee.Message + Environment.NewLine + Environment.NewLine + ee.ToString()); }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Icon = Properties.Resources.icon;
+            textBox1.Text = Goodfoto;
+            textBox2.Text = folder;
+        }
+
+        private void LinkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://ru.icons8.com");
+        }
+    }
+}
